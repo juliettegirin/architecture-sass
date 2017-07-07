9e5b93cc01836a21cdb7e112dbbe8cec94c32c7c
Juliette Girin <juliette.girin@lanetscouade.com>
2017-07-07 16:55:49 +0200

    update plugins / susy3 / new gulp tasks / add helpers and mixins

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,78 @@
+//imports + global variables
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 
 const $ = gulpLoadPlugins();
-const reload = browserSync.reload;
 
+var svgSprite = require('gulp-svg-sprite'),
+    reload = browserSync.reload;
+
+/* ------------- STYLE TASK -------------- */
+/* --------------------------------------- */
+/*  1. sass compiled to css
+    2. automatic vendor prefixs
+    3. combine all instances of media queries into one declaration
+*/
 gulp.task('styles', () => {
-  return gulp.src('app/styles/**/*.scss')
+  var autoprefixer = require('autoprefixer'),
+      pseudoContent = require('postcss-pseudo-content-insert'),
+      rucksack = require('rucksack-css');
+
+  return gulp.src('app/styles/*.scss')
+    .pipe($.plumber())
     .pipe($.sass.sync({
       outputStyle: 'expanded',
       precision: 10,
-      includePaths: ['./']
+      includePaths: ['.']
     }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-        browsers: ['last 2 versions', 'ie >= 10'],
-    }))
+    .pipe( $.postcss([
+        autoprefixer({browsers: ['last 2 versions', 'ie >= 11', 'Edge 13', 'Safari 7']}),
+        pseudoContent,
+        rucksack
+      ])
+    )
+    .pipe($.groupCssMediaQueries())
     .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
+    .pipe($.notify('Style task complete'))
+    .pipe(reload({'stream': true}));
 });
 
+gulp.task('scss-lint', () => {
+  var stylelint = require('stylelint'),
+      reporter = require('postcss-reporter'),
+      syntax_scss = require('postcss-scss');
+
+  var processors = [
+    stylelint(),
+    reporter({
+      clearMessages: true
+    })
+  ];
+
+  return gulp.src(['app/styles/**/*.scss', '!app/styles/abstracts/_helpers.scss', '!app/styles/layout/_form.scss',  '!app/styles/abstract/_mixins.scss'])
+    .pipe($.postcss(processors, {syntax: syntax_scss}));
+});
+
+//config for svg sprite
+const configSvgSprite = {
+  mode : {
+    symbol : {
+      dest            : '',
+      prefix          : 'icon-%s',
+      sprite          : 'icons.svg'
+    }
+  }
+}
+
+gulp.task('svg-sprite', () => {
+  gulp.src('app/images/icons/*.svg')
+      .pipe($.svgSprite(configSvgSprite))
+      .pipe(gulp.dest('images/'));
+});
+
+/* -- start server watch ---  */
+/* -------------------------- */
 gulp.task('serve', ['styles'], () => {
   browserSync({
     notify: false,
@@ -28,6 +82,10 @@ gulp.task('serve', ['styles'], () => {
     }
   });
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/styles/**/*.scss', ['styles', 'scss-lint']);
+  gulp.watch('app/images/icons/*.svg', ['svg-sprite']);
 });
+
+gulp.task('build', ['styles', 'images']);
+
 
